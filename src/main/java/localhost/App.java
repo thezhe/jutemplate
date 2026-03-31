cd2d3fba00ef881a18f95a3622ab494858b5d975
TheZhe <42720670+thezhe@users.noreply.github.com>
2026-03-31 00:24:38 -0400

    ABI

--- src/main/java/localhost/App.java
+++ src/main/java/localhost/App.java
@@ -3,7 +3,7 @@ package localhost;
 import java.io.IOException;
 import interfaces.ksy.Msg;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -14,6 +14,7 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import interfaces.schema.Config;
 import io.kaitai.struct.ByteBufferKaitaiStream;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.pubsub.RedisPubSubAdapter;
 import tools.jackson.databind.json.JsonMapper;
 
@@ -28,12 +29,10 @@ public final class App {
     private static final FluentLogger LOG = FluentLogger.forEnclosingClass();
 
     public static final class Listener
-            extends RedisPubSubAdapter<String, String> {
+            extends RedisPubSubAdapter<byte[], byte[]> {
         @Override
-        public void message(final String channel, final String message) {
-            try (var stream = new ByteBufferKaitaiStream(
-                    message.getBytes(Charset.defaultCharset()))) { // TODO
-                                                                   // encoding?
+        public void message(final byte[] channel, final byte[] message) {
+            try (var stream = new ByteBufferKaitaiStream(message)) {
                 final var msg = new Msg(stream);
                 IO.println(JsonMapper.shared().writeValueAsString(msg));
             } catch (IOException e) {
@@ -47,15 +46,19 @@ public final class App {
      * @see https://github.com/checkstyle/checkstyle/issues/17810
      */
     public static void main(final String... args) {
-        final var configPath = Path.of("./config.json");
+        final var configPath = Path.of("config.json");
         final var config = Files.exists(configPath)
                 ? JsonMapper.shared().readValue(configPath, Config.class)
                 : new Config();
         try (var client = RedisClient.create(config.getUri().orElseThrow());
-                var pubSubConnection = client.connectPubSub()) {
+                var pubSubConnection = client
+                        .connectPubSub(ByteArrayCodec.INSTANCE)) {
             pubSubConnection.addListener(new Listener());
             final var pubSub = pubSubConnection.sync();
-            pubSub.subscribe(config.getChannel().orElseThrow());
+            pubSub.subscribe(config
+                    .getChannel()
+                    .orElseThrow()
+                    .getBytes(StandardCharsets.UTF_8));
         }
     }
 
